bfb00e45df784f847cb9c5bd07d6039b4dbfbb68
i.avshtol <iharrwe@gmail.com>
2022-03-16 16:40:07 +0300

    Add logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import './App.css';
 import { Header } from './components/Header';
 import { Main } from './components/Main';
 import { SignInModal } from './components/SignInModal';
