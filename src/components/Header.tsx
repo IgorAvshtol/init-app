b389944a40596898e10b1f74394477364b724207
i.avshtol <iharrwe@gmail.com>
2022-03-24 21:16:17 +0300

    feature: Add useProvideAuth.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 import logo from '../image/logo.png';
 
-import { DropdownMenu } from './Menu';
+import { useAuth } from './hooks/useProvideAuth';
 
 interface IHeader {
   onSignInBtnClick: () => void;
@@ -8,36 +8,37 @@ interface IHeader {
 }
 
 export function Header({ onSignInBtnClick, onSignUpBtnClick }: IHeader) {
-  const image = 'https://api.realworld.io/images/smiley-cyrus.jpeg';
-  const auth = true;
-
+  const auth = useAuth();
   return (
     <header className="h-28 w-4/5 m-auto">
       <div className="h-full flex items-center">
-        <div className="w-full flex justify-between items-center relative">
+        <div className="w-full flex justify-between items-center">
           <a href="/">
             <img width={80} src={logo} alt="main-logo" />
           </a>
-          <div className="h-28 w-36 flex justify-between">
-            {auth ? (
-              <DropdownMenu image={image} />
-            ) : (
-              <div className="w-full flex justify-between">
-                <button
-                  className="text-green-300 hover:text-green-700 cursor-pointer"
-                  onClick={onSignInBtnClick}
-                >
-                  Sign In
-                </button>
-                <button
-                  className="text-green-300 hover:text-green-700 cursor-pointer"
-                  onClick={onSignUpBtnClick}
-                >
-                  Sign Up
-                </button>
-              </div>
-            )}
-          </div>
+          {auth.userData?.user ? (
+            <button
+              className="text-green-300 hover:text-green-700 cursor-pointer"
+              onClick={onSignInBtnClick}
+            >
+              {auth.userData.user.email}
+            </button>
+          ) : (
+            <div className="w-36 flex justify-between">
+              <button
+                className="text-green-300 hover:text-green-700 cursor-pointer"
+                onClick={onSignInBtnClick}
+              >
+                Sign In
+              </button>
+              <button
+                className="text-green-300 hover:text-green-700 cursor-pointer"
+                onClick={onSignUpBtnClick}
+              >
+                Sign Up
+              </button>
+            </div>
+          )}
         </div>
       </div>
       <hr className="pt-2" />
