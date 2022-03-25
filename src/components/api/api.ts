309ca237b338849a2d7223074de89685d8e7bc5a
i.avshtol <iharrwe@gmail.com>
2022-03-25 15:53:12 +0300

    fix: treatment error; feature: .env;

--- src/components/api/api.ts
+++ src/components/api/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const instance = axios.create({
   withCredentials: false,
-  baseURL: 'https://api.realworld.io/api/',
+  baseURL: process.env.REACT_APP_BASE_URL,
   headers: {
     'Content-Type': 'application/json',
   },
