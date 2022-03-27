132c529003b38121ef0edc9bc85f516181124f05
i.avshtol <iharrwe@gmail.com>
2022-03-27 14:28:37 +0300

    feature: Add DropDownMenu component;

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,31 +1,42 @@
 import logo from '../image/logo.png';
 
+import { DropdownMenu } from './Menu';
+
 interface IHeader {
   onSignInBtnClick: () => void;
   onSignUpBtnClick: () => void;
 }
 
 export function Header({ onSignInBtnClick, onSignUpBtnClick }: IHeader) {
+  const image = 'https://api.realworld.io/images/smiley-cyrus.jpeg';
+  const auth = true;
+
   return (
     <header className="h-28 w-4/5 m-auto">
       <div className="h-full flex items-center">
-        <div className="w-full flex justify-between items-center">
+        <div className="w-full flex justify-between items-center relative">
           <a href="/">
             <img width={80} src={logo} alt="main-logo" />
           </a>
-          <div className="w-36 flex justify-between">
-            <button
-              className="text-green-300 hover:text-green-700 cursor-pointer"
-              onClick={onSignInBtnClick}
-            >
-              Sign In
-            </button>
-            <button
-              className="text-green-300 hover:text-green-700 cursor-pointer"
-              onClick={onSignUpBtnClick}
-            >
-              Sign Up
-            </button>
+          <div className="h-28 w-36 flex justify-between">
+            {auth ? (
+              <DropdownMenu image={image} />
+            ) : (
+              <div className="w-full flex justify-between">
+                <button
+                  className="text-green-300 hover:text-green-700 cursor-pointer"
+                  onClick={onSignInBtnClick}
+                >
+                  Sign In
+                </button>
+                <button
+                  className="text-green-300 hover:text-green-700 cursor-pointer"
+                  onClick={onSignUpBtnClick}
+                >
+                  Sign Up
+                </button>
+              </div>
+            )}
           </div>
         </div>
       </div>
---
+++ src/components/Menu.tsx
@@ -0,0 +1,44 @@
+import { Menu } from '@headlessui/react';
+
+import menuLogo from '../image/menulogo.png';
+import avatar from '../image/avatar.png';
+
+interface IDropdownMenu {
+  image: string;
+}
+
+export function DropdownMenu({ image }: IDropdownMenu) {
+  return (
+    <Menu as="div">
+      <div className="absolute right-0 h-full flex items-center">
+        <Menu.Button>
+          <div className="w-28 flex items-center justify-between">
+            <img src={menuLogo} className="h-6 w-6" alt="menu" />
+            <img src={image || avatar} className="h-12" alt="avatar" />
+          </div>
+        </Menu.Button>
+      </div>
+      <Menu.Items className="bg-slate-100 rounded-xl border-inherit absolute top-24 right-0">
+        <div className="w-32 flex flex-col justify-items-start p-5">
+          <Menu.Item>
+            {({ active }) => (
+              <a className={`${active ? 'underline pb-2' : 'pb-2'}`} href="/account">
+                Settings
+              </a>
+            )}
+          </Menu.Item>
+          <Menu.Item disabled>
+            <hr className="bg-emerald-100 h-0.5" />
+          </Menu.Item>
+          <Menu.Item>
+            {({ active }) => (
+              <a className={`${active ? 'underline pt-2' : 'pt-2'}`} href="/">
+                Logout
+              </a>
+            )}
+          </Menu.Item>
+        </div>
+      </Menu.Items>
+    </Menu>
+  );
+}
