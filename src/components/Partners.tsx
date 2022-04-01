c368cc42c8d1e8aed2081277a332ff59149f8d6a
i.avshtol <iharrwe@gmail.com>
2022-04-01 18:56:54 +0300

    feature: add useFetch custom hook;

---
+++ src/components/Partners.tsx
@@ -0,0 +1,18 @@
+import microsoft from '../image/microsoft.jpg';
+import adobe from '../image/adobe.png';
+import saleforce from '../image/saleforce.png';
+import sap from '../image/sap.png';
+
+export function Partners() {
+  return (
+    <div className="w-4/5 pt-10 m-auto">
+      <h4 className="text-center text-2xl">Partners:</h4>
+      <div className="w-full flex flex-col items-center justify-center md:flex md:flex-row md:justify-between md:flex-wrap md:mt-8">
+        <img className="m-1" src={microsoft} width={220} alt="microsoft-logo" />
+        <img className="m-1" src={adobe} width={150} alt="adobe-logo" />
+        <img className="mt-4" src={saleforce} width={200} alt="saleforce-logo" />
+        <img className="mt-4" src={sap} width={250} alt="sap-logo" />
+      </div>
+    </div>
+  );
+}
--- src/components/Posts/Post.tsx
+++ src/components/Posts/Post.tsx
@@ -1,24 +1,29 @@
-import avatar from '../../image/avatar.png';
 import add from '../../image/add.svg';
 import lens from '../../image/lens.webp';
 
-export function Post() {
+interface IPost {
+  avatar: string;
+  title: string;
+  body: string;
+  author: string;
+  createdAt: string;
+}
+
+export function Post({ author, avatar, body, title, createdAt }: IPost) {
   return (
-    <div className="w-full pt-2 flex justify-around">
+    <div className="w-full pt-6 flex justify-around">
       <div className="w-2/3 flex flex-col">
         <a href="/" className="flex">
           <img src={avatar} width={25} height={20} alt="avatar" />
-          <p className="ml-2">Name</p>
+          <p className="ml-2">{author}</p>
         </a>
         <a href="/">
-          <h2 className="text-xl font-bold pt-2">In Defense Of The M-Dash</h2>
+          <h2 className="text-xl font-bold pt-2">{title}</h2>
         </a>
-        <h3 className="text-base font-normal text-zinc-400">
-          Writers worry they overuse it. They shouldn’ - it’s awesome
-        </h3>
+        <h3 className="text-base font-normal text-zinc-400">{body}</h3>
         <div className="flex justify-between items-center text-sm text-zinc-400">
           <div className="w-52 flex justify-between">
-            <span>Apr1</span>
+            <span>{createdAt}</span>
             <span>·</span>
             <span>3 min read</span>
             <span>·</span>
