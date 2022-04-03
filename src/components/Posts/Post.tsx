7a267b65cd969fc01c201466335183c41737c779
i.avshtol <iharrwe@gmail.com>
2022-04-03 20:11:11 +0300

    feat: add response in UI;

--- src/components/Posts/Post.tsx
+++ src/components/Posts/Post.tsx
@@ -15,37 +15,41 @@ interface IPost {
 export function Post(props: IPost) {
   const { author, avatar, description, title, createdAt, tagList, favoritesCount } = props;
   return (
-    <div className="w-full pt-6 flex justify-around">
+    <div className="w-full pt-6 flex justify-between">
       <div className="w-2/3 flex flex-col">
-        <div className="flex justify-between">
-          <a href="/" className="flex w-48">
+        <div className="flex justify-between w-44 xl:w-full lg:w-full md:w-full sm:w-full">
+          <a href="/" className="flex w-24">
             <img src={avatar} width={25} height={20} className="rounded-full" alt="avatar" />
             <p className="ml-2">{author}</p>
           </a>
           <a
             href="/"
-            className="flex w-20 justify-between items-center px-2 bg-emerald-100 text-center rounded-full border-black"
+            className="flex w-[65px] justify-between items-center px-1 bg-emerald-100 text-center rounded-full border-black"
           >
             <img className="h-4" src={like} alt="favourite" />
-            <p>{favoritesCount}</p>
+            <p className="text-xs xl:text-base lg:text-base md:text-base sm:text-base">
+              {favoritesCount}
+            </p>
           </a>
         </div>
         <a href="/">
-          <h2 className="text-xl font-bold pt-2">{title}</h2>
+          <h2 className="text-base font-bold pt-2 xl:text-xl lg:text-xl md:text-xl sm:text-base">
+            {title}
+          </h2>
         </a>
-        <h3 className="text-base font-normal text-zinc-400">{description}</h3>
-        <div className="flex justify-between items-center text-sm text-zinc-400">
-          <div className="w-52 flex justify-between">
+        <h3 className="text-sm font-normal text-zinc-400 xl:text-base lg:text-base md:text-base sm:text-xs">
+          {description}
+        </h3>
+        <div className="pt-2 flex justify-between items-start text-xs text-zinc-400">
+          <div className="w-full flex">
             <span>{createdAt}</span>
-            <span>·</span>
-            <span>3 min read</span>
-            <span>·</span>
-            <a href="/" className="w-20">
+            <span className="pl-1">·</span>
+            <a href="/" className="w-full flex flex-wrap">
               {tagList.map((tag, index) => {
                 return (
                   <span
                     key={index}
-                    className="ml-1 px-2 bg-zinc-200 text-center rounded-full border-black"
+                    className="mb-1 ml-1 px-2 bg-zinc-200 text-center rounded-full border-black flex justify-center"
                   >
                     {tag}
                   </span>
@@ -54,12 +58,12 @@ export function Post(props: IPost) {
             </a>
           </div>
           <a href="/">
-            <img src={add} height={25} width={25} className="fill-white" alt="favourite" />
+            <img src={add} className="fill-white w-5 xl:w-6 lg:w-6 md:w-6 sm:w-6" alt="favourite" />
           </a>
         </div>
       </div>
       <a href="/">
-        <img src={lens} width={200} height={134} alt="post-cover" />
+        <img src={lens} alt="post-cover" className="w-24 xl:w-44 lg:w-44 md:w-44 sm:w-40" />
       </a>
     </div>
   );
--- src/components/Posts/Posts.tsx
+++ src/components/Posts/Posts.tsx
@@ -7,14 +7,14 @@ import { useFetch } from '../hooks/useFetch';
 export function Posts() {
   const { articles, loading, error } = useFetch();
   return (
-    <div className="w-1/2 flex flex-col justify-items-start">
+    <div className="w-full pt-6 flex flex-col justify-items-start xl:w-2/3 lg:w-2/3 md:w-full sm:w-full sm:pt-2">
       {error ? (
         <div className="w-full h-full flex justify-center items-center">
           <img src={notFound} alt="not-found" />
         </div>
       ) : loading ? (
         <div className="w-full h-full flex justify-center items-center">
-          <img src={spinner} width={100} height={100} alt="spinner" />
+          <img src={spinner} width={100} alt="spinner" />
         </div>
       ) : (
         articles?.map((post) => {
