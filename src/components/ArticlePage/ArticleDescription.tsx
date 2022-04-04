dab3022486e1ce5a5514ed1b03940d881ddfa1f3
i.avshtol <iharrwe@gmail.com>
2022-04-04 23:05:52 +0300

    feature: add Image.tsx

--- src/components/ArticlePage/ArticleDescription.tsx
+++ src/components/ArticlePage/ArticleDescription.tsx
@@ -1,4 +1,4 @@
-import lens from '../../image/lens.webp';
+import { Image } from './Image';
 
 interface IArticleDescription {
   title: string;
@@ -11,7 +11,7 @@ export function ArticleDescription({ title, description, body }: IArticleDescrip
     <div className="mt-4 w-full flex-col">
       <p className="text-4xl">{title}</p>
       <p className="text-xl">{description}</p>
-      <img src={lens} className="w-full pt-10" alt="image" />
+      <Image />
       <p className="text-lg pt-10">{body}</p>
     </div>
   );
