3ad089f8a2e161f6f7585bffde9108810362d199
i.avshtol <iharrwe@gmail.com>
2022-04-05 13:58:25 +0300

    fix: add pseudo-classes for some buttons;

--- src/components/Article/Navbar.tsx
+++ src/components/Article/Navbar.tsx
@@ -4,10 +4,16 @@ import notification from '../../image/notification.png';
 export function Navbar() {
   return (
     <div className="mt-96 w-full sticky flex-col top-24">
-      <a href="/">
+      <a
+        href="/"
+        className="relative hover:after:content-['Home'] after:absolute after:top-2 after:left-10 after:rounded-lg after:px-1 after:bg-emerald-100"
+      >
         <img className="w-9" src={home} alt="home" />
       </a>
-      <a href="/">
+      <a
+        href="/"
+        className="relative hover:after:content-['Notification'] after:absolute after:top-6 after:left-10 after:rounded-lg after:px-1 after:bg-emerald-100"
+      >
         <img className="w-6 ml-1.5 mt-6" src={notification} alt="notification" />
       </a>
     </div>
