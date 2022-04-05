8322565dc6aa60175a46bd4775e747bfcaffe8d3
i.avshtol <iharrwe@gmail.com>
2022-04-05 15:55:43 +0300

    fix: add ability pass additional parameters in useFetch hook; delete: filterArticles function;

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -3,24 +3,24 @@ import { useState, useEffect, useCallback } from 'react';
 import { fetchDataService } from '../services/getResponseDataService';
 import { TypeLoadingStatus } from '../interfaces/interfaces';
 
-export const useFetch = <T>(url: string) => {
+export const useFetch = <T>(url: string, options?: string) => {
   const [data, setData] = useState<T | null>(null);
   const [loading, setLoading] = useState<TypeLoadingStatus>(TypeLoadingStatus.IS_RESOLVE);
   const [error, setError] = useState(false);
   const getResponseData = useCallback(() => {
     setLoading(TypeLoadingStatus.IS_PENDING);
-    fetchDataService(url)
+    fetchDataService(url, options)
       .then((res) => {
         setLoading(TypeLoadingStatus.IS_RESOLVE);
         const urlKey = url.split('').splice(1, url.length).join('');
-        setData(res.data[urlKey]);
+        setData(options ? res.data : res.data[urlKey]);
       })
       .catch((err) => {
         console.log(err);
         setLoading(TypeLoadingStatus.IS_REJECTED);
         setError(true);
       });
-  }, [url]);
+  }, [url, options]);
 
   useEffect(() => {
     getResponseData();
