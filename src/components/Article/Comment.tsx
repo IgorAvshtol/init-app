72f83472580e134d6ecdcca1643a3668f71fd168
i.avshtol <iharrwe@gmail.com>
2022-04-11 16:28:56 +0300

    fix: implemented auto-height for textarea using useref and useeffect;

--- src/components/Article/Comment.tsx
+++ src/components/Article/Comment.tsx
@@ -1,25 +1,52 @@
 import { formatDistance } from 'date-fns';
+import { AxiosResponse } from 'axios';
+import { useState } from 'react';
 
-import { IAuthor } from 'interfaces';
+import { IAuthor, IEditCommentData } from 'interfaces';
+import { EditMenu } from './EditMenu';
+import { useAuth } from 'hooks/useProvideAuth';
 
 interface ICommentProps {
   createdAt: string;
   body: string;
   author: IAuthor;
+  id: number;
+  deleteComment: (url: string) => Promise<AxiosResponse | string>;
 }
 
-export function Comment({ author, createdAt, body }: ICommentProps) {
+export function Comment({ author, createdAt, body, id }: ICommentProps) {
+  const { user } = useAuth();
+  const currentUser = user?.user.username;
+  const [editMenuIsShow, setEditMenuIsShow] = useState<IEditCommentData>({
+    id: null,
+    isEdit: false,
+  });
   const correctDate = formatDistance(new Date(createdAt), new Date(), {
     addSuffix: true,
   });
+  const onEditButtonHandler = (id: number) => {
+    setEditMenuIsShow((prevState) => ({ id: id, isEdit: !prevState.isEdit }));
+  };
   return (
     <div className="mt-4 flex-col relative">
-      <div className="flex">
-        <img src={author.image} className="w-12 rounded-full" alt="avatar" />
-        <div className="pl-2">
-          <p className="font-medium text-lg">{author.username}</p>
-          <p className="text-sm text-zinc-400">{correctDate}</p>
+      <div className="w-full flex justify-between">
+        <div className="flex">
+          <img src={author.image} className="w-12 rounded-full" alt="avatar" />
+          <div className="pl-2">
+            <p className="font-medium text-lg">{author.username}</p>
+            <p className="text-sm text-zinc-400">{correctDate}</p>
+          </div>
         </div>
+        {currentUser === author.username && (
+          <>
+            <button className="font-normal" onClick={() => onEditButtonHandler(id)}>
+              ···
+            </button>
+            {editMenuIsShow.id === id && editMenuIsShow.isEdit && (
+              <EditMenu id={id} setEditMenuIsShow={setEditMenuIsShow} />
+            )}
+          </>
+        )}
       </div>
       <div className="my-2 text-sm">{body}</div>
       <hr className="mt-9 w-full" />
