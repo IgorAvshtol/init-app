0f1f8a28192c2dfc6bb45f8f677005bc4f12cfef
i.avshtol <iharrwe@gmail.com>
2022-04-13 22:53:50 +0300

    fix: forbidden non-null in Animation.tsx;

--- src/components/style/Animation.tsx
+++ src/components/style/Animation.tsx
@@ -4,14 +4,17 @@ export function Animation() {
   const fadingHeading = document.querySelectorAll('.fading');
   for (let a = 0; a < fadingHeading.length; a++) {
     const letters = fadingHeading[a].textContent?.split('');
-    const content = letters!.map((val: string) => {
-      const delay = Math.floor(Math.random() * 30000 + 1);
-      return '<span style="animation-delay: ' + delay + 'ms">' + val + '</span>';
-    });
+    const content =
+      letters &&
+      letters.map((val) => {
+        const delay = Math.floor(Math.random() * 30000 + 1);
+        return '<span style="animation-delay: ' + delay + 'ms">' + val + '</span>';
+      });
     fadingHeading[a].innerHTML = ' ';
-    for (let i = 0; i < content.length; i++) {
-      fadingHeading[a].innerHTML += content[i];
-    }
+    if (content)
+      for (let i = 0; i < content.length; i++) {
+        fadingHeading[a].innerHTML += content[i];
+      }
   }
   return (
     <div className="absolute h-[390px] z-10 right-0 hidden xl:block lg:block md:block sm:static">
