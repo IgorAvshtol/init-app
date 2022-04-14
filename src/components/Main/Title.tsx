491d21af1afea83d5a8682762d749b078d49923c
i.avshtol <iharrwe@gmail.com>
2022-04-14 11:36:43 +0300

    fix: update UI Title, Header components; feat: add PrivateRoute;

--- src/components/Main/Title.tsx
+++ src/components/Main/Title.tsx
@@ -1,16 +1,18 @@
-import { Animation } from '../style/Animation';
+import { Animation } from '../Style/Animation';
 
 export function Title() {
   return (
     <div className="relative w-full mt-12 bg-emerald-400 flex justify-end items-center h-[450px] border-b-[1px] border-black -z-10">
-      <div className="w-5/6 mx-auto xl:ml-[20%] lg:ml-[10%] md:ml-[10%]">
-        <h1 className="text-5xl xl:text-8xl lg:text-7xl md:text-7xl sm:text-7xl">Itransition.</h1>
-        <h4 className="mt-4 text-2xl xl:text-3xl lg:text-3xl md:text-3xl sm:text-3xl">
+      <div className="relative w-3/4 m-auto flex-col justify-between items-center xl:w-3/5 lg:w-5/6 md:w-5/6 sm:w-5/6 z-10">
+        <h1 className="text-5xl xl:text-8xl lg:text-7xl md:text-7xl sm:text-7xl font-serif	">
+          Itransition.
+        </h1>
+        <h4 className="mt-10 font-light text-2xl xl:text-2xl lg:text-2xl md:text-3xl sm:text-3xl">
           We’re a global software engineering company <br /> making success stories for over 20
           years
         </h4>
-        <button className="mt-4 bg-emerald-600 hover:bg-slate-400 text-white font-bold py-2 px-4 rounded-full">
-          Start
+        <button className="mt-10 bg-emerald-600 text-xl	text-white w-52 py-2 px-4 rounded-full">
+          Start reading
         </button>
       </div>
       <Animation />
