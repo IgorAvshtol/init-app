6c85969ac71443704ae171cb439cc6c8c9d8315f
i.avshtol <iharrwe@gmail.com>
2022-04-15 09:52:18 +0300

    fix: rename sendComment into createComment;

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -41,11 +41,6 @@ export interface IBody {
   body: string;
 }
 
-export interface IEditCommentData {
-  id?: number | null;
-  isEdit: boolean;
-}
-
 export enum TypeLoadingStatus {
   IS_PENDING = 'IS_PENDING',
   IS_RESOLVED = 'IS_RESOLVED',
