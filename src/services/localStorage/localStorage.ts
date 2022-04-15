106cb6353ca1c09d4dae2db991bcba1824df1c3f
i.avshtol <iharrwe@gmail.com>
2022-04-15 09:35:04 +0300

    fix: remake dataFromStorageIsValid into getUserFromLocalStorage;

--- src/services/localStorage/localStorage.ts
+++ src/services/localStorage/localStorage.ts
@@ -1,7 +1,16 @@
 import { IResponseData } from 'hooks/useProvideAuth';
 
+const REACT_APP_LOCALSTORAGE_KEY = 'userData';
+
 export const getUserFromLocalStorage = () => {
-  return localStorage.getItem(`${process.env.REACT_APP_LOCALSTORAGE_KEY}`);
+  const data = localStorage.getItem(REACT_APP_LOCALSTORAGE_KEY);
+  if (data) {
+    try {
+      return JSON.parse(data);
+    } catch (e) {
+      console.log('JSON is not valid');
+    }
+  }
 };
 
 export const setUserFromLocalStorage = (data: IResponseData) => {
